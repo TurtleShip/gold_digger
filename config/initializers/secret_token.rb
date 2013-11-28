4cf4e77294ecf747b8740f5dfa8ad4369add3733
TurtleShip <sk765@cornell.edu>
2013-11-28 03:33:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GoldDigger::Application.config.secret_key_base = 'bbe77ccdee5ca11be052bb6725076c8b47a47fd6a9df9ff2f9e863e80103335b6cb501f3e1eaba3a8c9b541f8e7259142f8cde8357db86a73b63cb24fb15ac3a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GoldDigger::Application.config.session_store :cookie_store, key: '_gold_digger_session'
