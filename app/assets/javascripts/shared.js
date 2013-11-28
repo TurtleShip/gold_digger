22761e61793e6749d58390450b70c7df487c108d
TurtleShip <sk765@cornell.edu>
2013-11-28 06:30:46 -0500

    _menu is updated to properly show the active menu. simulator.js in progress

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -3,14 +3,3 @@
  * all pages. You shouldn't be putting javascript codes here
  * if at least one page doesn't require them.
  */
-
-function showSlectedMenu() {
-    var path = window.location.pathname;
-    var link = $('a[href="' + path +'"]');
-    link.parent().addClass('active');
-}
-
-// Starts function on page load
-$(function() {
-    showSlectedMenu();
-})
--- app/assets/javascripts/simulator.js
+++ app/assets/javascripts/simulator.js
@@ -1,3 +1,44 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+
+var canvas; // simulation will be drawn on this canvas
+var board; // 2D array that stores locations of each component
+
+var board_width = 50;
+var board_height = 50;
+
+var bot_total = 25;
+var gen_total = 100;
+
+var move = [[1,0],[-1,0],[0,1],[0,-1]];
+
+// All possible elements that can be placed on board
+var empty = 0, mine = -1, bot = 2;
+
+board = new Array(board_height);
+for(var i = 0; i < board_height; i++) {
+    board[i] = new Array(board_width);
+}
+
+// Create a new board
+function createBoard() {
+
+    for(var row = 0; row < board_height; row++) {
+        for(var col = 0; col < board_width; col++) {
+            // start out empty for now
+            board[row][col] = empty;
+        }
+    }
+
+    // Add bots on the top row
+    for(var col = 0; col < board_width; col++) {
+        board[0][col] = bot;
+    }
+}
+
+
+//$(function() {
+//    canvas = $("#canvas");
+//    // make sure that we are in the correct page
+//    if( canvas.length == 0 ) {
+//        return 0;
+//    }
+//})
